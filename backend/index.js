836101ffaff39f55acb101a57fdafa82a54b3f8c
preetikumari15 <annukumari0928@gmail.com>
2025-05-25 00:11:32 +0530

    Api setup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/connectionDB.js';
+import userRoutes from './routes/user.routes.js';
 dotenv.config();
 
 const app = express();
@@ -9,9 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
-    res.send('Hello');
-})
+//API ENDPOINTS
+app.use("/images", express.static("uploads"));
+app.use("/user", userRoutes);
 const PORT = 4000;
 app.listen(PORT,()=>{
     connectDB();
